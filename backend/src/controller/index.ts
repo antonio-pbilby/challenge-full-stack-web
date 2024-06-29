9324869809fb38cb17f51b87e06d4f7502b88eff
Antônio Lemos <antoniobilbylemos@gmail.com>
2024-06-28 22:50:14 -0300

    fix: class instances and error handling

--- backend/src/controller/index.ts
+++ backend/src/controller/index.ts
@@ -1,12 +1,16 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import type { UserService } from "../services/user.service";
 
 export class UserController {
 	constructor(private userService: UserService) {}
 
-	async create(req: Request, res: Response) {
-		const userData = req.body;
-		await this.userService.create(userData);
-		return res.status(201).send();
+	async create(req: Request, res: Response, next: NextFunction) {
+		try {
+			const userData = req.body;
+			await this.userService.create(userData);
+			return res.status(201).send();
+		} catch (err) {
+			next(err);
+		}
 	}
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,8 +2,16 @@ import { Router } from "express";
 import { UserController } from "./controller";
 import { validateRequest } from "./middlewares/validation.middleware";
 import { createUserSchema } from "./schemas/user.schema";
+import { UserRepository } from "./repositories/user.repository";
+import { UserService } from "./services/user.service";
 
 export const router = Router();
-const userController = new UserController();
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
 
-router.post("/user", validateRequest(createUserSchema), userController.create);
+router.post(
+	"/user",
+	validateRequest(createUserSchema),
+	userController.create.bind(userController),
+);
